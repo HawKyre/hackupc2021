9378569ebba969f1ff8b6e6da1c519a6fe085e28
HawKyre <50209142+HawKyre@users.noreply.github.com>
2021-05-15 13:05:34 +0200

    Add head with fonts

---
+++ src/components/Head.js
@@ -0,0 +1,13 @@
+const Head = () => {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap"
+        rel="stylesheet"
+      ></link>
+    </>
+  );
+};
+
+export default Head;
