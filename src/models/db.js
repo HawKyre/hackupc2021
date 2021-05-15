51b2756b2f6eced0e7fe6f5e8834f3240a0a9e06
HawKyre <50209142+HawKyre@users.noreply.github.com>
2021-05-15 15:13:06 +0200

    Fix database path

--- src/models/db.js
+++ src/models/db.js
@@ -1,15 +1,14 @@
-import sqlite from "sqlite";
-import sqlite3 from "sqlite3";
+const sqlite = require("sqlite");
+const sqlite3 = require("sqlite3");
 
 let db;
 
-export default function getDB() {
+export default async function getDB() {
   if (!db) {
-    sqlite
-      .open({ filename: "../../db.sqlite", driver: sqlite3.Database })
-      .then((database) => {
-        db = database;
-      });
+    db = await sqlite.open({
+      filename: "./db.sqlite",
+      driver: sqlite3.Database,
+    });
   }
   return db;
 }
