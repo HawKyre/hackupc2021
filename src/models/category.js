0af3d8a97f9eab3fb22852560e87ba68941ad5dd
adriavc00 <adriavciscar@gmail.com>
2021-05-15 23:04:42 +0200

    implement of getCategoryByID

---
+++ src/models/category.js
@@ -0,0 +1,29 @@
+import getDB from "@models/db";
+
+export const getCategoryByID = (id) => {
+  let error = false;
+  const db = await getDB();
+  let response;
+  const query =
+    "SELECT Category.*, Uni.name as uni_name FROM Category INNER JOIN Uni ON Category.uni_id = Uni.id WHERE Category.id = ?";
+  try {
+    response = await db.get(query, [1]);
+  } catch (e) {
+    error = e;
+  }
+  if (!response || error) {
+    return { success: false, error: error || "No data." };
+  }
+  const { id, name, uni_id, uni_name } = response;
+  return {
+    success: true,
+    data: {
+      id,
+      name,
+      uni: {
+        uni_id,
+        name: uni_name,
+      },
+    },
+  };
+};
