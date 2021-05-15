a7eb827b775550b820015515aaaed1c6013f8efd
adriavc00 <adriavciscar@gmail.com>
2021-05-15 16:15:44 +0200

    Create university implementation with db

--- src/models/university.js
+++ src/models/university.js
@@ -1,13 +1,29 @@
 import getDB from "@models/db";
 
 export default class Uni {
-  constructor(name_, categories) {
+  constructor(name_, id) {
     this.name = name_;
-    this.categories = categories;
+    this.id = id;
+  }
+
+  get categories() {
+    return (async () => {
+      return JSON.parse(
+        await getDB().all("SELECT name FROM Category WHERE uni_id = ?", [
+          this.id,
+        ])
+      );
+    })();
   }
 }
 
 export const getUnisListFromDB = async () => {
-  const db = await getDB();
-  return await db.all("SELECT name FROM Uni");
+  const response = JSON.parse(await getDB().all("SELECT name, id FROM Uni"));
+  if (!response) {
+    return { success: false, content: null };
+  }
+  return {
+    success: true,
+    content: response.map(({ name: name_, id }) => new Uni(name_, id)),
+  };
 };
