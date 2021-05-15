526e64ae4fd17980b62141b1a6918e8bdb64c744
adriavc00 <adriavciscar@gmail.com>
2021-05-15 18:47:15 +0200

    modify users.js to implement model communnicated with db

--- src/models/user.js
+++ src/models/user.js
@@ -1,37 +1,93 @@
+import getDB from "./db";
+
 export default class User {
   constructor(
+    id,
     nickname,
     email,
     passwd,
     uni,
     name = undefined,
-    surname = undefined,
+    surnames = undefined,
     degree = undefined
   ) {
+    this.id = id;
     this.nickname = nickname;
     this.email = email;
     this.passwd = passwd;
     this.uni = uni;
     this.data = {
       name,
-      surname,
+      surnames,
       degree,
     };
   }
+
+  async JSON() {
+    return JSON.parse({
+      nickname: this.nickname,
+      email: this.email,
+      passwd: this.passwd,
+      data: this.data,
+    });
+  }
+
+  /* getCreatedPosts */
 }
 
-export const getUserFromDB = (email, passwd) => {
-  return new User("Joe", email, passwd, "UPV");
-};
+export const getUserFromDB = (nickname, passwd) => {
+  let error = false;
+  const query =
+    "SELECT User.id, User.nickname, User.email, User.passwd, User.uni_id, Uni.name, User.first_name, User.surnames, User.degree FROM User INNER JOIN Uni ON User.id = Uni.id WHERE User.nickname = ? AND User.passwd = ?";
+  const response = await getDB().get(
+    query,
+    [nickname, passwd],
+    (e) => (error = e)
+  );
 
-export const createUserInDB = (email, passwd) => {
-  return new User("Joe", email, passwd, "UPV");
-};
-
-export const toJSON = (user) => {
+  if (!response || error) {
+    return { success: false, error: error || "No data" };
+  }
   return {
-    nickname: user.nickname,
-    email: user.email,
-    passwd: user.passwd,
+    success: true,
+    data: _getUserFromResponse(response),
   };
+};
+
+export const createUserInDB = (
+  nickname,
+  email,
+  passwd,
+  uni_id,
+  name = null, // Null values to avoid problems inserting it to the db.
+  surnames = null,
+  degree = null
+) => {
+  let error = false;
+  let query =
+    "INSERT INTO User (nickname, email, passwd, uni_id, first_name, surnames, degree) VALUES (?, ?, ?, ?, ?, ?, ?)";
+  let response = await db.run(
+    query,
+    [nickname, email, passwd, uni_id, name, surnames, degree],
+    (e) => (error = e)
+  );
+  query = "SELECT * FROM User WHERE id = ?";
+  response = await db.get(query, response.lastID);
+  if (!response || false) {
+    return { success: false, error: error || "Failed to insert." };
+  }
+  return { success: true, data: _getUserFromResponse(response) };
+};
+
+const _getUserFromResponse = (response) => {
+  const {
+    id,
+    nickname: nick,
+    email,
+    passwd: password,
+    name: uni,
+    uni_id,
+    ...data
+  } = response;
+  return new User(id, nick, email, password, new Uni(uni_id, uni), ...data);
 };
