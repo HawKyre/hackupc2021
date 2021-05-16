7054e4bd49779441c09bdddc80567b2088072be2
adriavc00 <adriavciscar@gmail.com>
2021-05-16 04:13:51 +0200

    Integrate PWA in the app

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+  },
   env: {
     JWT_SECRET: "laksdjksafhuisdoif89 8wer 98t439843t89yuweiuhiuhj",
   },
-};
+});
