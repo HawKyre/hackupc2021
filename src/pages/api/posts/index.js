525c8e7be333317c529b28ccb15b0afcd05ca096
HawKyre <50209142+HawKyre@users.noreply.github.com>
2021-05-16 06:38:24 +0200

    Modify posts api route

--- src/pages/api/posts/index.js
+++ src/pages/api/posts/index.js
@@ -1,8 +1,33 @@
 import { getCommentsFromDB } from "@models/comments";
-import { getCategoryPostsFromDB, getPostByID } from "@models/post";
+import {
+  createPostInDB,
+  getCategoryPostsFromDB,
+  getPostByID,
+} from "@models/post";
 
 export default async (req, res) => {
   switch (req.method) {
+    case "POST": {
+      const { authorID, categoryID, title, content } = req.body;
+      const newPost = await createPostInDB(
+        authorID,
+        categoryID,
+        title,
+        content
+      );
+      if (newPost.success) {
+        res.status(301).json({
+          success: true,
+          data: newPost.data,
+        });
+      } else {
+        res.status(400).json({
+          success: false,
+          error: newPost.error,
+        });
+      }
+      break;
+    }
     case "GET": {
       if (req.query.categoryID) {
         const categoryID = req.query.categoryID;
