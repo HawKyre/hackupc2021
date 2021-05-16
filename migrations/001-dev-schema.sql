955b37a2a8dbbb813449068dd5d95ea063b673c8
adriavc00 <adriavciscar@gmail.com>
2021-05-16 05:11:39 +0200

    showcase databse

--- migrations/001-dev-schema.sql
+++ migrations/001-dev-schema.sql
@@ -33,36 +33,58 @@ CREATE TABLE Category (
     name VARCHAR(255),
     uni_id INTEGER REFERENCES Uni(id)
 );
-INSERT INTO User (nickname, email, passwd, uni_id)
-VALUES ("foo", "ex@email.com", "foo", 1);
-INSERT INTO User (nickname, email, passwd, uni_id)
-VALUES ("a", "a", "a", 1);
-INSERT INTO User (
-        nickname,
-        email,
-        passwd,
-        uni_id,
-        first_name,
-        surnames,
-        degree
-    )
-VALUES (
-        "bar",
-        "ex@email.com",
-        "foo",
-        1,
-        "juan",
-        "andres lopez",
-        "la calle"
-    );
 INSERT INTO Uni (name)
 VALUES ("UPV");
-INSERT INTO Post (author_id, category_id, title, content)
-VALUES (1, 1, "cositas", "se vienen cositas");
+INSERT INTO Uni (name)
+VALUES ("UPC");
 INSERT INTO Category (name, uni_id)
-VALUES ("trap", 1);
+VALUES ("Deportes", 1);
+INSERT INTO Category (name, uni_id)
+VALUES ("Becas", 1);
+INSERT INTO Category (name, uni_id)
+VALUES ("Fiesta", 1);
+INSERT INTO Category (name, uni_id)
+VALUES ("Gaming", 1);
+INSERT INTO Category (name, uni_id)
+VALUES ("Noticias", 1);
+INSERT INTO Category (name, uni_id)
+VALUES ("General", 1);
+INSERT INTO Category (name, uni_id)
+VALUES ("Esports", 2);
+INSERT INTO Category (name, uni_id)
+VALUES ("Beques", 2);
+INSERT INTO Category (name, uni_id)
+VALUES ("Festa", 2);
+INSERT INTO Category (name, uni_id)
+VALUES ("Gaming", 2);
+INSERT INTO Category (name, uni_id)
+VALUES ("Noticies", 2);
+INSERT INTO Category (name, uni_id)
+VALUES ("General", 2);
+INSERT INTO User (nickname, email, passwd, uni_id)
+VALUES ("adriavc00", "foo@email.com", "foo", 1);
+INSERT INTO User (nickname, email, passwd, uni_id)
+VALUES ("hawkyre", "bar@email.com", "bar", 2);
+INSERT INTO Post (author_id, category_id, title, content)
+VALUES (
+        1,
+        1,
+        "Partida de tenis",
+        "Me gustaria organizar una partida de tenis para el próximo lunes a las 17:00. ¿Hay alguien interesado en jugar conmigo?"
+    );
+INSERT INTO Post (author_id, category_id, title, content)
+VALUES (
+        2,
+        10,
+        "Competició e-sport",
+        "He escoltat que s'està organitzant una competició de videojocs, però no tinc molta més informació. Algú sap més informació?"
+    );
 INSERT INTO Comment (author_id, post_id, content)
-VALUES (2, 1, "ice");
+VALUES (
+        2,
+        2,
+        "He descobert que es tracta d'una competició del LOL. Si algú està interessat en que li passe el link que em comente."
+    );
 -- Down
 DROP TABLE User;
 DROP TABLE Post;
